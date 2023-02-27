e0ab689dcd2246ae67fbddf49c695f6fcfb0c773
Sejal Jaiswal <710sejal@gmail.com>
2023-02-27 21:46:44 +0530

    main

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 const messageRoutes = require("./routes/messages");
 const app = express();
+const socket = require("socket.io");
+require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect("mongodb+srv://sejal:sejal@cluster0.iv2p69r.mongodb.net/chat?retryWrites=true&w=majority", {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -24,8 +26,8 @@ mongoose
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-const server = app.listen(8080, () =>
-  console.log(`Server started on 8080`)
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
 );
 const io = socket(server, {
   cors: {
